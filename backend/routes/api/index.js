a87a800ac15babe3b1ad5b22ebab37bfae9c5e30
Jon Granados <grxnxdos@gmail.com>
2022-12-13 23:55:25 -0800

    Implemented and tested middleware that will aid backend authentication flow.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router(); 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', (req, res) => { 
     res.json( { requestBody: req.body }); 
 }); 
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+// GET /api/restore-user
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// GET /api/require-auth
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 module.exports = router; 
