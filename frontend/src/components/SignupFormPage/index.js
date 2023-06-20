2bf1714cdd41a2b84136122ee528437e3850bdac
Jon Granados <grxnxdos@gmail.com>
2023-06-20 13:05:18 -0700

    feat: implement form submission handler

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
+import * as sessionActions from '../../store/sessionSlice';
 import "./SignupForm.css";
 
 import { Field, Form, Formik } from "formik";
 import { initialSignupValues } from "../../validations";
-import { Link } from "react-router-dom";
 
 export default function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   if (sessionUser) return <Redirect to="/" />;
 
-  const handleSubmit = async (event) => {
+  const handleFormSubmit = async (values) => {
+    const { username, email, password, confirmPassword } = values; 
+
+    if (password === confirmPassword) {
+      return dispatch(sessionActions.signup({ email, username, password }))
+        .unwrap()
+        .catch(async (backendValidationErrors) => {
+          alert(backendValidationErrors);
+        });
+	  }
   };
 
   return (
-		<Formik initialValues={initialSignupValues}>
-			{({ values, handleChange, handleBlur }) => (
-				<Form>
+		<Formik 
+      initialValues={initialSignupValues}
+      onSubmit={handleFormSubmit}>
+			{({ values, handleChange, handleBlur, handleSubmit }) => (
+				<Form onSubmit={handleSubmit}>
 					<label htmlFor="username">Username</label>
 					<Field
 						name="username"
