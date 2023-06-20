d0dd7099060e10f38f96706d7d2b945691fc1715
Jon Granados <grxnxdos@gmail.com>
2023-06-20 12:39:04 -0700

    feat: implement basic signup page using Formik

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,85 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import * as sessionActions from "../../store/sessionSlice";
 import "./SignupForm.css";
 
+import { Field, Form, Formik } from "formik";
+import { initialSignupValues } from "../../validations";
 
 export default function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password }))
-        .unwrap()
-        .catch(async (backendValidationErrors) => {
-          setErrors(backendValidationErrors)
-        });
-    }
-
-    return setErrors(['Password fields must match']); 
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => {
-          return <li key={idx}>{error}</li>;
-        })}
-      </ul>
-      <div className="credentials">
-        <div>
-          <label>
-            Username
-            <input
-              type="text"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Email
-            <input
-              type="text"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input
-              type="pasword"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Confirm Password
-            <input
-              type="pasword"
-              value={confirmPassword}
-              onChange={(event) => setConfirmPassword(event.target.value)}
-              required
-            />
-          </label>
-        </div>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+		<Formik initialValues={initialSignupValues}>
+			{({ values, handleChange, handleBlur }) => (
+				<Form>
+					<label htmlFor="username">Username</label>
+					<Field
+						name="username"
+						id="username"
+						type="text"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.username}
+					/>
+
+					<label htmlFor="email">Email</label>
+					<Field
+						name="email"
+						id="email"
+						type="email"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.email}
+					/>
+
+					<label htmlFor="password">Password</label>
+					<Field
+						name="password"
+						id="password"
+						type="text"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.password}
+					/>
+
+					<label htmlFor="confirmPassword">Confirm password</label>
+					<Field
+						name="confirmPassword"
+						id="confirmPassword"
+						type="password"
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.confirmPassword}
+					/>
+				</Form>
+			)}
+		</Formik>
   );
 };
