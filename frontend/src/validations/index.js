1eb2f44a491dd8701c3e10bfe31fd4f9b8ba8e1a
Jon Granados <grxnxdos@gmail.com>
2023-06-20 13:15:03 -0700

    feat: update error messages in yup schemas

--- frontend/src/validations/index.js
+++ frontend/src/validations/index.js
@@ -4,8 +4,8 @@ import * as yup from "yup";
 export const loginValidationSchema = yup.object().shape({
 	credential: yup
 		.string()
-		.required("Your username or email address is required."),
-	password: yup.string().required("Your password is required."),
+		.required("Your username or email address is required"),
+	password: yup.string().required("Your password is required"),
 });
 
 export const initialLoginValues = {
@@ -17,24 +17,24 @@ export const initialLoginValues = {
 export const signupValidationSchema = yup.object().shape({
 	username: yup
 		.string()
-		.min(4, "Your username must be at least 4 characters long.")
-		.max(30, "Your username must be less than 30 characters long.")
-		.required("required"),
+		.min(4, "Your username must be at least 4 characters long")
+		.max(30, "Your username must be less than 30 characters long")
+		.required("Choose a username"),
 	email: yup
 		.string()
 		.email("Your email must be a valid email.")
-		.min(3, "Your password must be between 3 and 56 characters long.")
-		.max(56, "Your password must be between 3 and 56 characters long.")
-		.required("required"),
+		.min(3, "Your password must be between 3 and 56 characters long")
+		.max(56, "Your password must be between 3 and 56 characters long")
+		.required("Your email is required"),
 	password: yup
 		.string()
-		.min(6, "Your password must be between 6 and 20 characters long.")
-		.max(20, "Your password must be between 6 and 20 characters long.")
+		.min(6, "Your password must be between 6 and 20 characters long")
+		.max(20, "Your password must be between 6 and 20 characters long")
 		.required("Your password is required"),
 	confirmPassword: yup
 		.string()
-		.oneOf([yup.ref("password"), null], "Your passwords must match.")
-		.required("Confirm your password."),
+		.oneOf([yup.ref("password"), null], "Your passwords must match")
+		.required("Confirm your password"),
 });
 
 export const initialSignupValues = {
