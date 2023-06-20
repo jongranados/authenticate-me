6d16dcdf9cb94bbe166bbc57dce7cfbbe7d54d5c
Jon Granados <grxnxdos@gmail.com>
2023-06-19 21:55:43 -0700

    feat: implement basic Formik login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,58 +1,63 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
-import * as sessionActions from "../../store/sessionSlice";
+import { Redirect, Link } from "react-router-dom";
 import "./LoginForm.css";
+
+import { Formik, Form, Field } from "formik";
+import { initialLoginValues, loginValidationSchema } from '../../validations';
+
 
 export default function LoginFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
 
-  const [credential, setCredential] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-
   if (sessionUser) return <Redirect to="/" />;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setErrors([]);
-    return dispatch(sessionActions.login({ credential, password })).catch(
-      async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      }
-    );
+  const handleFormSubmit = (event) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => {
-          return <li key={idx}>{error}</li>;
-        })}
-      </ul>
-      <div className="credentials">
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(event) => setCredential(event.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="pasword"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-          />
-        </label>
-      </div>
-      <button type="submit">Log In</button>
-    </form>
+
+		<Formik
+			onSubmit={handleFormSubmit}
+			initialValues={initialLoginValues}
+			validationSchema={loginValidationSchema}
+		>
+			{({
+				values,
+				handleBlur,
+				handleChange,
+				handleSubmit,
+			}) => (
+				<Form
+					onSubmit={handleSubmit}
+				>
+					<label htmlFor="credential">Username or email</label>
+					<Field
+						id="credential"
+						name="credential"
+						type="text"
+						onBlur={handleBlur}
+						onChange={handleChange}
+						value={values.credential}
+					/>
+
+					<label htmlFor="password">Password</label>
+					<Field
+						id="password"
+						name="password"
+						type="password"
+						onBlur={handleBlur}
+						onChange={handleChange}
+						value={values.password}
+					/>
+
+					<button type="submit">LOGIN</button>
+					<Link to="/signup">
+						Don't have an account? Sign up here!
+					</Link>
+				</Form>
+			)}
+		</Formik>
   );
 }
