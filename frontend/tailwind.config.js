ae668aa5855ed33cc05c5e6d40a01f207e1fce0b
Jon Granados <grxnxdos@gmail.com>
2023-06-25 20:07:00 -0700

    chore: import Google fonts and update font family in tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Poppins", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+				roboto: ["Roboto", "sans-serif"], 
+				bayon: ["Bayon", "sans-serif"]
 			},
 		},
 	},
