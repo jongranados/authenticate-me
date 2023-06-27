459935c61567065a94e77df02ae5dfee51954b66
Jon Granados <grxnxdos@gmail.com>
2023-06-27 11:48:15 -0700

    style: style login form headings

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Link } from "react-router-dom";
 import * as sessionActions from '../../store/sessionSlice'; 
+import abstractPapercut from './abstractPapercut.jpg'
+
 import "./LoginForm.css";
 
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -22,50 +24,62 @@ export default function LoginFormPage() {
 		.unwrap() 
 		.catch(async backendValidationErrors => alert(backendValidationErrors)); 
   };
-
   return (
-		<Formik
-			onSubmit={handleFormSubmit}
-			initialValues={initialLoginValues}
-			validationSchema={loginValidationSchema}
-		>
-			{({
-				values,
-				errors,
-				touched,
-				handleBlur,
-				handleChange,
-				handleSubmit,
-			}) => (
-				<Form onSubmit={handleSubmit}>
-					<label htmlFor="credential">Username or email</label>
-					<Field
-						id="credential"
-						name="credential"
-						type="text"
-						onBlur={handleBlur}
-						onChange={handleChange}
-						value={values.credential}
-					/>
-					<ErrorMessage name="credential" />
-					
-					<label htmlFor="password">Password</label>
-					<Field
-						id="password"
-						name="password"
-						type="password"
-						onBlur={handleBlur}
-						onChange={handleChange}
-						value={values.password}
-					/>
-					<ErrorMessage name="password" />
+		<div className="flex flex-row">
+			<div className="w-full lg:w-1/2 border-solid border-2 border-indigo-600">
+			</div>
 
-					<button type="submit">LOGIN</button>
-					<Link to="/signup">
-						Don't have an account? Sign up here!
-					</Link>
-				</Form>
-			)}
-		</Formik>
+			<div className="w-full px-4 mb-16 lg:w-1/2 lg:mb-0 border-solid border-2 border-indigo-600">
+				<div className="max-w-lg mx-auto lg:pt-8 lg:pb-8">
+					<h3 className="mb-4 text-5xl text-gray-900 font-bold">Welcome Back!</h3>
+					<p className="mb-15 text-lg text-gray-500">Login below if you already have an account.</p>
+					<Formik
+						onSubmit={handleFormSubmit}
+						initialValues={initialLoginValues}
+						validationSchema={loginValidationSchema}
+					>
+						{({
+							values,
+							errors,
+							touched,
+							handleBlur,
+							handleChange,
+							handleSubmit,
+						}) => (
+							<Form onSubmit={handleSubmit}>
+								<label htmlFor="credential">
+									Username or email
+								</label>
+								<Field
+									id="credential"
+									name="credential"
+									type="text"
+									onBlur={handleBlur}
+									onChange={handleChange}
+									value={values.credential}
+								/>
+								<ErrorMessage name="credential" />
+
+								<label htmlFor="password">Password</label>
+								<Field
+									id="password"
+									name="password"
+									type="password"
+									onBlur={handleBlur}
+									onChange={handleChange}
+									value={values.password}
+								/>
+								<ErrorMessage name="password" />
+
+								<button type="submit">LOGIN</button>
+								<Link to="/signup">
+									Don't have an account? Sign up here!
+								</Link>
+							</Form>
+						)}
+					</Formik>
+				</div>
+			</div>
+		</div>
   );
 }
