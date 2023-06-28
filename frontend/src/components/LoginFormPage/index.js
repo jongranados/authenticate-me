f200b16a3635c5a0021b767df2afa1192f7fc530
Jon Granados <grxnxdos@gmail.com>
2023-06-27 22:57:14 -0700

    style: style login page form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -26,13 +26,16 @@ export default function LoginFormPage() {
   };
   return (
 		<div className="flex flex-row">
-			<div className="w-full lg:w-1/2 border-solid border-2 border-indigo-600">
-			</div>
+			<div className="w-full lg:w-1/2 border-solid border-2 border-indigo-600"></div>
 
-			<div className="w-full px-4 mb-16 lg:w-1/2 lg:mb-0 border-solid border-2 border-indigo-600">
-				<div className="max-w-lg mx-auto lg:pt-8 lg:pb-8">
-					<h3 className="mb-4 text-5xl text-gray-900 font-bold">Welcome Back!</h3>
-					<p className="mb-15 text-lg text-gray-500">Login below if you already have an account.</p>
+			<div className="w-full px-4 pb-16 lg:w-1/2 lg:mb-0 border-solid border-2 border-indigo-600">
+				<div className="max-w-lg mx-auto lg:pt-24 lg:pb-4">
+					<h3 className="mb-2 text-6xl text-gray-900 font-bold">
+						Welcome Back!
+					</h3>
+					<p className="mb-8 text-lg font-light text-gray-500">
+						Login below to access your account.
+					</p>
 					<Formik
 						onSubmit={handleFormSubmit}
 						initialValues={initialLoginValues}
@@ -47,34 +50,75 @@ export default function LoginFormPage() {
 							handleSubmit,
 						}) => (
 							<Form onSubmit={handleSubmit}>
-								<label htmlFor="credential">
-									Username or email
-								</label>
-								<Field
-									id="credential"
-									name="credential"
-									type="text"
-									onBlur={handleBlur}
-									onChange={handleChange}
-									value={values.credential}
-								/>
-								<ErrorMessage name="credential" />
+								<div className="my-6">
+									<label
+										htmlFor="credential"
+										className="block mb-1.5 text-sm font-light text-gray-900"
+									>
+										Username or email
+									</label>
+									<Field
+										className="w-full px-4 py-3 text-sm font-light text-gray-900 border border-gray-200 rounded-lg focus:shadow-lg focus:border-[#FF5F01] focus:outline-none"
+										id="credential"
+										name="credential"
+										type="text"
+										onBlur={handleBlur}
+										onChange={handleChange}
+										value={values.credential}
+									/>
+									<div className="h-2">
+										<ErrorMessage
+											name="credential"
+											component="div"
+											className="pt-1 text-xs font-extralight text-red-600"
+										/>
+									</div>
+								</div>
 
-								<label htmlFor="password">Password</label>
-								<Field
-									id="password"
-									name="password"
-									type="password"
-									onBlur={handleBlur}
-									onChange={handleChange}
-									value={values.password}
-								/>
-								<ErrorMessage name="password" />
+								<div className="my-6">
+									<label
+										htmlFor="password"
+										className="block mb-1.5 text-sm font-light text-gray-900"
+									>
+										Password
+									</label>
+									<Field
+										className="w-full px-4 py-3 text-sm font-light text-gray-900 border border-gray-200 rounded-lg focus:shadow-lg focus:border-[#FF5F01] focus:outline-none"
+										id="password"
+										name="password"
+										type="password"
+										onBlur={handleBlur}
+										onChange={handleChange}
+										value={values.password}
+									/>
+									<div className="h-2">
+										<ErrorMessage
+											name="password"
+											component="div"
+											className="pt-1 text-xs font-extralight text-red-600"
+										/>
+									</div>
+								</div>
 
-								<button type="submit">LOGIN</button>
-								<Link to="/signup">
-									Don't have an account? Sign up here!
-								</Link>
+								<div className="my-8 flex justify-center">
+									<button
+										type="submit"
+										className="w-full px-8 py-4 text-sm rounded-xl font-semibold text-white bg-[#FF5F01] hover:bg-orange-600"
+									>
+										LOGIN
+									</button>
+								</div>
+
+								<div className="mt-20 text-gray-900 font-light text-sm text-center">
+									Don't have an account?
+									<Link
+										to="/signup"
+										className="font-bold text-[#FF5F01] hover:text-blue-800"
+									>
+										{" "}
+										Create one here!
+									</Link>
+								</div>
 							</Form>
 						)}
 					</Formik>
