e68db59eaa19e4e84b68c5c0137f8330115adb69
Jon Granados <grxnxdos@gmail.com>
2023-07-03 16:47:29 -0700

    feat: remove redundant nav controls from ux prior to user being authorized

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,33 +1,22 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 export default function Navigation({sessionUser}) {
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = <ProfileButton user={sessionUser} />;
-  } else {
-    sessionLinks = (
-      <div>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink className="signup-link" to="/signup">Sign Up</NavLink>
-      </div>
-    );
-  }
+  let sessionLinks = <ProfileButton user={sessionUser} />;
 
-  return (
+  return sessionUser ? (
 		<>
 			<ul>
-				<li className="navigation-wrapper">
-					<NavLink exact to="/">
-						Home
-					</NavLink>
-					{sessionLinks}
-				</li>
+				<li>{sessionLinks}</li>
 			</ul>
-      <Outlet/>
+			<Outlet />
+		</>
+  ) : (
+		<>
+			<Outlet />
 		</>
   );
 };
