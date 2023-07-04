3e55c52d64c0f34fca0886e9ea016f2dbac8de1a
Jon Granados <grxnxdos@gmail.com>
2023-07-04 15:54:11 -0700

    style: Layout component

--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Navigation from "../Navigation";
 export default function Layout({ sessionUser }) { 
 
     return (
-			<div className="min-h-screen flex flex-col justify-center">
+			<div className="min-h-screen flex flex-col justify-start">
                 <Navigation sessionUser={ sessionUser }/>
                 <Outlet/>
 			</div>
