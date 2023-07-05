bef72010bdfbce157ec40355623313869d648f2b
Jon Granados <grxnxdos@gmail.com>
2023-07-04 20:07:00 -0700

    style: center application vertically

--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Navigation from "../Navigation";
 export default function Layout({ sessionUser }) { 
 
     return (
-			<div className="min-h-screen flex flex-col justify-start">
+			<div className="min-h-screen flex flex-col justify-center">
                 <Navigation sessionUser={ sessionUser }/>
                 <Outlet/>
 			</div>
